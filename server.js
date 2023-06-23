90de1b75a4ebdc42f8b392c367e56aca2f7efb4f
Ramankauldhar <rmnkaul979697@gmail.com>
2023-06-23 13:55:24 -0400

    V3: MongoDB connection established

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const { chats } = require("./chat");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 dotenv.config();
 
 //creating an express api
@@ -19,3 +20,13 @@ app.get("/chats/:id", (req, res) => {
 //created a node server, which is running on port :5000 or 2019
 const PORT = process.env.PORT || 2023;
 app.listen(PORT, console.log(`Server started on Port:${PORT}`));
+
+//calling the db connection function
+dbconn().catch((err) => console.log(err));
+
+async function dbconn() {
+  await mongoose.connect(process.env.MONGODB_CONN);
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  console.log("Mongo DB connected");
+}
